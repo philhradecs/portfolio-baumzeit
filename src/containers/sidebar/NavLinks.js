20a801dc5a91ac6c15cb535c629eb471def8d377
Philip Hradecsni <baumzeit@posteo.me>
2019-01-11 16:00:38 +0100

    change NavLinks component to use the router's location object

--- src/containers/sidebar/NavLinks.js
+++ src/containers/sidebar/NavLinks.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from '@reach/router';
+import { Link, Location } from '@reach/router';
 import styled from 'styled-components';
 
 const FlexLinks = styled.nav`
@@ -10,14 +10,19 @@ const FlexLinks = styled.nav`
   height: 100%;
 `
 
-const getPathObj = () => ({ prevPath: window.location.pathname })
-
 export default () => (
-  <FlexLinks>
-    <Link className="link" to="/" state={getPathObj()}>Home</Link>
-    <Link className="link" to="/projects" state={getPathObj()}>Projects</Link>
-    <Link className="link" to="/mind" state={getPathObj()}>Mind</Link>
-    <Link className="link" to="/cv" state={getPathObj()}>CV</Link>
-    <Link className="link" to="/contact" state={getPathObj()}>Contact</Link>
-  </FlexLinks>
+  <Location>
+    {({ location }) => {
+      const getPathObj = () => ({ prevPath: location.pathname });
+      return (
+        <FlexLinks>
+          <Link className="link" to="/" state={getPathObj()}>Home</Link>
+          <Link className="link" to="/projects" state={getPathObj()}>Projects</Link>
+          <Link className="link" to="/mind" state={getPathObj()}>Mind</Link>
+          <Link className="link" to="/cv" state={getPathObj()}>CV</Link>
+          <Link className="link" to="/contact" state={getPathObj()}>Contact</Link>
+        </FlexLinks>
+      )
+    }}
+  </Location>
 );
