03982936085b1f866f60adcd50909f4d556a94fa
Philip Hradecsni <baumzeit@posteo.me>
2019-01-20 15:02:34 +0100

    implement asynchronous font loading

--- src/loadWebFonts.js
+++ src/loadWebFonts.js
@@ -1,16 +1,17 @@
-import WebFont from 'webfontloader';
-
 export default () => {
   if (sessionStorage.getItem('fonts')) {
     document.documentElement.classList.add('wf-active');
   }
 
-  WebFont.load({
-    google: {
-      families: ['Titillium Web:400,600']
-    },
-    active: () => {
-      sessionStorage.setItem('fonts', true);
-    }
-  });
-};
+  import('webfontloader').then(WebFont => {
+    WebFont.load({
+      google: {
+        families: ['Titillium Web:400,600']
+      },
+      active: () => {
+        sessionStorage.setItem('fonts', true);
+      }
+    })
+  })
+}
+
