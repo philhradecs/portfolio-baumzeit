0308c6bd1304f0ad31bed35d0ea4ea9d01cedb99
Philip Hradecsni <baumzeit@posteo.me>
2019-01-29 12:55:44 +0100

    define expanded html head via static.config

--- static.config.js
+++ static.config.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 export default {
   plugins: ['react-static-plugin-styled-components'],
+  getSiteData: async () => ({
+    title: 'Portfolio Baumzeit'
+  }),
   siteRoot: 'https://portfolio-baumzeit.netlify.com',
-  Document: ({ Html, Head, Body, children, siteData, renderMeta }) => (
+  Document: ({ Html, Head, Body, children, siteData }) => (
     <Html lang="en-US">
       <Head>
+        <title>{siteData.title}</title>
+        <meta name="author" content="Philip Hradecsni" />
+        <meta name="lastBuilt" content={siteData.lastBuilt} />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" type="text/css" href="/styles/fontAwesome.css" />
