acd5b88be3384265b64171e82631a29a3279f493
Philip Hradecsni <baumzeit@posteo.me>
2019-01-31 00:32:49 +0100

    rename assets, clean up package.json

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -52,10 +52,10 @@ export default () => (
       <Bold>Click image to download as PDF →</Bold>
     </Paragraph>
     <FlexContainer>
-      <Anchor href='/cv/Philip_Hradecsni_CV.pdf'>
+      <Anchor href='/cv_assets/public_cv_philip_hradecsni.pdf'>
         <ImageWrapper>
-          <Image src='/cv/Philip_Hradecsni_CV-1.jpg' left />
-          <Image src='/cv/Philip_Hradecsni_CV-2.jpg' right />
+          <Image src='/cv_assets/Philip_Hradecsni_CV-1.jpg' left />
+          <Image src='/cv_assets/Philip_Hradecsni_CV-2.jpg' right />
         </ImageWrapper>
       </Anchor>
     </FlexContainer>
