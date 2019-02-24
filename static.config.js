bbc0ec744867cec3d950d16e855466d60f976590
Philip Hradecsni <baumzeit@posteo.me>
2019-02-24 16:32:05 +0100

    optimize favicon compatibility

--- static.config.js
+++ static.config.js
@@ -11,12 +11,18 @@ export default {
     <Html lang="en-US">
       <Head>
         <title>{siteData.title}</title>
-        <link rel="shortcut icon" type="image/png" href="/favicon.png"/>
         <meta name="author" content="Philip Hradecsni" />
         <meta name="lastBuilt" content={siteData.lastBuilt} />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" type="text/css" href="/styles/fontAwesome.css" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#2b5797" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <Body>{children}</Body>
     </Html>
