b836f9fd354ca9cb7e7f590f191452f7fc0fe94b
Philip Hradecsni <baumzeit@posteo.me>
2019-03-24 22:34:55 +0100

    prepare for production

--- src/styles/getThemeForPath.js
+++ src/styles/getThemeForPath.js
@@ -1,14 +1,11 @@
 import routeThemes from './routeThemes';
 
 export default (pathname) => {
-    console.log('passed pathname: ' + pathname)
     if (!pathname) {
         return routeThemes.default;
     }
     const re = /(.+)\/$/;
     const sanitizedPath = pathname.replace(re, '$1');
-
-    console.log('sanitized path: ' + sanitizedPath)
     
     if (Object.prototype.hasOwnProperty.call(routeThemes, sanitizedPath)) {
         return routeThemes[sanitizedPath]
